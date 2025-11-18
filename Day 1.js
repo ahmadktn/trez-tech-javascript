4842503b6f776c8853652a262d6930eeeab81a0a
AhmadBB <90705524+ahmadktn@users.noreply.github.com>
2025-11-18 11:12:24 +0100

    Add console logs and variable declaration for user info

--- Day 1.js
+++ Day 1.js
@@ -1,1 +1,10 @@
+console.log("Ahmad");
+console.log("21");
+console.log("Red");
 
+//  Create variables for name, age, and school, then print them together in one sentence.
+
+var name = "Ahmad";
+var age = 21;
+var school = "Bayero University Kano";
+document.writeln("my name is " + name + ", I am " + age + " years old and I study at " + school);
