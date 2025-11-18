c37e73e98e6eab97ad3848185f1d1d3ae459fa29
AhmadBB <90705524+ahmadktn@users.noreply.github.com>
2025-11-18 11:15:46 +0100

    Print favorite fruits and numbers from 1 to 20

---
+++ Day 4.js
@@ -0,0 +1,11 @@
+// Create an array of your 5 favorite fruits and print each one using a for loop.
+let fruits = ["Mango", "Banana", "Apple", "Orange", "Pineapple"];
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+// Write a while loop that prints numbers from 1 to 20.
+let i = 1;
+while(i<=20){
+    document.writeln(i);
+}
