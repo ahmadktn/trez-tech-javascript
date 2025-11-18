77258e0a9ec203befcedfd9e975460b331b9e376
AhmadBB <90705524+ahmadktn@users.noreply.github.com>
2025-11-18 11:16:55 +0100

    Fix multiplication table logging for number 5

---
+++ Day 5.js
@@ -0,0 +1,14 @@
+// Write a loop that prints all even numbers between 1 and 50.
+for(let i=1; i<=50; i++){
+    if(i%2 == 0){
+        console.log(i);
+    }
+    else{
+        continue;
+    }
+}
+
+// Use a for loop to print the multiplication table of any number (e.g., 5).
+for(let number = 1; number<=12; number++){
+    console.log("5 x " + number + " =" + " " + 5*number);
+}
