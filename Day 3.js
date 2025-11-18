2de9a2355263ee7b1234b063d9d34ff22b3d5b22
AhmadBB <90705524+ahmadktn@users.noreply.github.com>
2025-11-18 11:14:21 +0100

    Add number and voting eligibility check programs

---
+++ Day 3.js
@@ -0,0 +1,20 @@
+// Write a program that checks if a number is positive, negative, or zero.
+let num = prompt("Enter a number");
+if (num > 0){
+    console.log("The number is positive");
+}
+else if (num < 0){
+    console.log("the number is negative");
+}
+else{
+    console.log("the number is zero");
+}
+
+// Write a program that checks if a person is old enough to vote (age ≥ 18).
+let age = prompt("Enter your age");
+if (age >= 18){
+    console.log("You are eligible to vote");
+}
+else{
+    console.log("You are not eligible to vote");
+}
