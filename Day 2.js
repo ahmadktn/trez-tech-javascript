f0e94d5eb3ff6d546f4ab8a2564ca8a71e91d248
AhmadBB <90705524+ahmadktn@users.noreply.github.com>
2025-11-18 11:13:18 +0100

    Create Day 2.js

---
+++ Day 2.js
@@ -0,0 +1,13 @@
+// Create variables of different data types (string, number, boolean)
+var name = "Ahmad";
+var age = 21;
+var confirm = true;
+
+// Use arithmetic operators (+, -, *, /, %) to calculate and display results in the console.
+var num1 = 55;
+var num2 = 10;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
